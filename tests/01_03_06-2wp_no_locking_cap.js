1ce560216eb86b56f8eb18edb3dbe76b51651ef0
Jan Pikora <honza@Jans-MacBook-Pro.local>
2023-08-31 20:40:43 +0200

    refactor of 01_03_06-2wp_no_locking_cap.js

--- tests/01_03_06-2wp_no_locking_cap.js
+++ tests/01_03_06-2wp_no_locking_cap.js
@@ -1,116 +1,79 @@
 const expect = require('chai').expect
-var { sequentialPromise, wait } = require('../lib/utils');
-const peglib = require('peglib');
-const bitcoin = peglib.bitcoin;
-const rsk = peglib.rsk;
-const pegUtils = peglib.pegUtils;
-const pegAssertions = require('../lib/assertions/2wp');
-const whitelistingAssertionsLegacy = require('../lib/assertions/whitelisting-legacy');
-const CustomError = require('../lib/CustomError');
 const rskUtils = require('../lib/rsk-utils');
 const { getRskTransactionHelpers } = require('../lib/rsk-tx-helper-provider');
 const { getBtcClient } = require('../lib/btc-client-provider');
+const { sendPegin, ensurePeginIsRegistered, sendTxToBridge, MINIMUM_PEGIN_VALUE_IN_BTC } = require('../lib/2wp-utils');
+const { getBridge, getLatestActiveForkName } = require('../lib/precompiled-abi-forks-util');
+const { getDerivedRSKAddressInformation } = require('@rsksmart/btc-rsk-derivation');
+const btcEthUnitConverter = require('btc-eth-unit-converter');
+const whitelistingAssertions = require('../lib/assertions/whitelisting');
 
 describe('Transfer BTC to RBTC before papyrus200', function() {
 
-  var federationAddress;
-  var btcClient;
-  var rskClient;
-  var pegClient;
-  var test;
-  let whitelistTestLegacy;
   let rskTxHelpers;
   let btcTxHelper;
+  let rskTxHelper;
 
-  const NETWORK = bitcoin.networks.testnet;
+  const MINIMUM_PEGIN_VALUE_IN_SATOSHI = btcEthUnitConverter.btcToSatoshis(MINIMUM_PEGIN_VALUE_IN_BTC)
+  const MINIMUM_PEGIN_VALUE_IN_WEIS = btcEthUnitConverter.btcToWeis(MINIMUM_PEGIN_VALUE_IN_BTC)
+ 
+  const fulfillRequirementsToRunAsSingleTestFile = async () => {
+    await rskUtils.activateFork(Runners.common.forks.wasabi100);
+  };
   
   before(async () => {
-    btcClient = bitcoin.getClient(
-      Runners.hosts.bitcoin.rpcHost,
-      Runners.hosts.bitcoin.rpcUser,
-      Runners.hosts.bitcoin.rpcPassword,
-      NETWORK
-    );
-    rskClient = rsk.getClient(Runners.hosts.federate.host);
-    rskClients = Runners.hosts.federates.map(federate => rsk.getClient(federate.host));
-    pegClient = pegUtils.using(btcClient, rskClient);
-    test = pegAssertions.with(btcClient, rskClient, pegClient);
-    whitelistTestLegacy = whitelistingAssertionsLegacy.with(btcClient, rskClient, pegClient);
-    rskTxHelpers = getRskTransactionHelpers();
-    btcTxHelper = getBtcClient();
+      rskTxHelpers = getRskTransactionHelpers();
+      rskTxHelper = rskTxHelpers[0];
+      btcTxHelper = getBtcClient()
 
-    // Grab the federation address
-    federationAddress = await rskClient.rsk.bridge.methods.getFederationAddress().call();
-    await btcClient.importAddress(federationAddress, 'federations');
-    
-    // Mine a few rsk blocks to prevent being at the beginning of the chain,
-    // which could trigger border cases we're not interested in
-    await sequentialPromise(10, () => rskUtils.mineAndSync(rskTxHelpers));
-    
-    // // Update the bridge to sync btc blockchains
-    await rskClient.fed.updateBridge();
-    await rskUtils.mineAndSync(rskTxHelpers);
+      if(process.env.RUNNING_SINGLE_TEST_FILE) {
+          await fulfillRequirementsToRunAsSingleTestFile();
+      }
+  })
 
-    return federationAddress;
-  });
 
   it('should transfer BTC to RBTC', async () => {
-    try {
-      var initialFedBalance = (await btcClient.getAddressBalance(federationAddress))[federationAddress] || 0;
-      expect(initialFedBalance).to.be.lessThan(bitcoin.btcToSatoshis(1000));
-      
-      const INITIAL_RSK_BALANCE = bitcoin.btcToSatoshis(1000) + bitcoin.btcToSatoshis(1);
-      const INITIAL_BTC_BALANCE = INITIAL_RSK_BALANCE + bitcoin.btcToSatoshis(1);
-      const TO_BRIDGE_GAS_PRICE = 1;
-      const WEIS_TO_RELEASE = '500000000000000000000';
+    const btcAddressInfo = await btcTxHelper.generateBtcAddress('legacy');
+    await whitelistingAssertions.assertAddLimitedLockWhitelistAddress(rskTxHelper, btcAddressInfo.address, 3 * MINIMUM_PEGIN_VALUE_IN_SATOSHI);
+    await rskUtils.mineAndSync(rskTxHelpers);
+    
+    const recipientRskAddressInfo = getDerivedRSKAddressInformation(btcAddressInfo.privateKey, btcTxHelper.btcConfig.network);
+    await rskTxHelper.importAccount(recipientRskAddressInfo.privateKey);
+    const unlocked = await rskTxHelper.unlockAccount(recipientRskAddressInfo.address);
+    expect(unlocked, 'Account was not unlocked').to.be.true;
 
-      var addresses = await pegClient.generateNewAddress('test');
-      expect(addresses.inRSK).to.be.true;
+    const latestActiveForkName = await getLatestActiveForkName();
+    const bridge = getBridge(rskTxHelper.getClient(), latestActiveForkName);
+    const federationAddress = await bridge.methods.getFederationAddress().call();
 
-      await whitelistTestLegacy.assertAddLimitedLockWhitelistAddress(addresses.btc, INITIAL_BTC_BALANCE)();
-      await btcClient.sendToAddress(addresses.btc, INITIAL_BTC_BALANCE);
-      await btcClient.generate(1);
-      await test.assertBitcoinBalance(addresses.btc, INITIAL_BTC_BALANCE, "Wrong initial BTC balance");
-      await wait(1000);
-      await test.assertLock(addresses, [{ address: federationAddress, amount: INITIAL_RSK_BALANCE }]);
+    const federationAddressBalanceInitial = Number(await btcTxHelper.getAddressBalance(federationAddress));
 
-      var lockedFedBalance = Number((await btcClient.getAddressBalance(federationAddress))[federationAddress]);
-      expect(lockedFedBalance).to.be.greaterThan(bitcoin.btcToSatoshis(1000) + initialFedBalance);
+    await btcTxHelper.fundAddress(btcAddressInfo.address, 3 * MINIMUM_PEGIN_VALUE_IN_BTC + btcTxHelper.getFee());
+    
+    const btcPeginTxHash = await sendPegin(rskTxHelper, btcTxHelper, btcAddressInfo, 3 * MINIMUM_PEGIN_VALUE_IN_BTC);
+    await ensurePeginIsRegistered(rskTxHelper, btcPeginTxHash);
+    const recipientRskAddressBalanceAfterPegin = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address))
+    expect(Number(recipientRskAddressBalanceAfterPegin)).to.be.equal(Number(3 * MINIMUM_PEGIN_VALUE_IN_WEIS));
 
-      await rskClient.eth.personal.unlockAccount(addresses.rsk, '');
+    await sendTxToBridge(rskTxHelper, MINIMUM_PEGIN_VALUE_IN_BTC, recipientRskAddressInfo.address);
+    await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
 
-      //Need to call sendTx in order to make it always with value < 1000, otherwise it fails
-      await rskClient.rsk.sendTx({
-        from: addresses.rsk,
-        to: rsk.getBridgeAddress(),
-        value: WEIS_TO_RELEASE,
-        gasPrice: TO_BRIDGE_GAS_PRICE
-      }, rskClient.evm.mine);
+    const federationAddressBalanceAfterFirstPegout = Number(await btcTxHelper.getAddressBalance(federationAddress));
+    expect(Number(federationAddressBalanceAfterFirstPegout)).to.be.equal(Number(federationAddressBalanceInitial + 2 * MINIMUM_PEGIN_VALUE_IN_BTC));
 
-      //Call release after sendTx so requests are released. Goes one by one
-      await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
+    const senderAddressBalanceAfterFirstPegout = Number(await btcTxHelper.getAddressBalance(btcAddressInfo.address));
+    expect(Number(senderAddressBalanceAfterFirstPegout)).to.be.above(MINIMUM_PEGIN_VALUE_IN_BTC - btcTxHelper.getFee()).and.below(MINIMUM_PEGIN_VALUE_IN_BTC);
 
-      await wait(500);
-      
-      var afterFirstReleaseFedBalance = Number((await btcClient.getAddressBalance(federationAddress))[federationAddress]);
-      expect(afterFirstReleaseFedBalance).to.be.at.most(INITIAL_BTC_BALANCE / 2 + initialFedBalance);
+    const recipientRskAddressAfterFirstPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address))
+    expect(Number(recipientRskAddressAfterFirstPegout)).to.be.above(MINIMUM_PEGIN_VALUE_IN_WEIS - btcTxHelper.getFee()).and.below(2 * MINIMUM_PEGIN_VALUE_IN_WEIS);
+    
+    await sendTxToBridge(rskTxHelper, MINIMUM_PEGIN_VALUE_IN_BTC, recipientRskAddressInfo.address);
+    await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
 
-      //Need to call sendTx in order to make it always with value < 1000, otherwise it fails
-      await rskClient.rsk.sendTx({
-        from: addresses.rsk,
-        to: rsk.getBridgeAddress(),
-        value: WEIS_TO_RELEASE,
-        gasPrice: TO_BRIDGE_GAS_PRICE
-      }, rskClient.evm.mine);
+    const senderAddressBalanceAfterSecondPegout = Number(await btcTxHelper.getAddressBalance(btcAddressInfo.address));
+    expect(Number(senderAddressBalanceAfterSecondPegout)).to.be.above(2*MINIMUM_PEGIN_VALUE_IN_BTC - 2*btcTxHelper.getFee()).and.below(2*MINIMUM_PEGIN_VALUE_IN_BTC);
 
-      //Call release after sendTx so requests are released. Goes one by one
-      await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
-      
-      var finalFedBalance = Number((await btcClient.getAddressBalance(federationAddress))[federationAddress]);
-      expect(finalFedBalance).to.be.at.most(initialFedBalance + bitcoin.btcToSatoshis(1));
-    } 
-    catch (err) {
-      throw new CustomError('Transfer BTC to RBTC failure', err);
-    }
+    const recipientRskAddressAfterSecondPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address))
+    expect(Number(recipientRskAddressAfterSecondPegout)).to.be.below(MINIMUM_PEGIN_VALUE_IN_WEIS);
   });
 });
