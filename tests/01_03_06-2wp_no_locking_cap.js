50b99608f59af214fe06056022ae601d8383edc0
Jan Pikora <honza@Jans-MacBook-Pro.local>
2023-09-08 18:28:13 +0200

    asserts update

--- tests/01_03_06-2wp_no_locking_cap.js
+++ tests/01_03_06-2wp_no_locking_cap.js
@@ -16,9 +16,7 @@ describe('Transfer BTC to RBTC before papyrus200', function() {
 
   const PEGIN_VALUE_IN_BTC = 3 * MINIMUM_PEGIN_VALUE_IN_BTC;
   const PEGOUT_VALUE_IN_RBTC = PEGIN_VALUE_IN_BTC / 3;
-  const PEGOUT_VALUE_IN_WEIS = btcEthUnitConverter.btcToWeis(PEGOUT_VALUE_IN_RBTC);
   const RSK_TX_FEE_IN_RBTC = 0.001;
-  const FINAL_SENDER_BALANCE_APPROX = 2 * PEGOUT_VALUE_IN_RBTC - 2 * RSK_TX_FEE_IN_RBTC
  
   const fulfillRequirementsToRunAsSingleTestFile = async () => {
     await rskUtils.activateFork(Runners.common.forks.wasabi100);
@@ -62,22 +60,22 @@ describe('Transfer BTC to RBTC before papyrus200', function() {
     await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
 
     const federationAddressBalanceAfterFirstPegout = Number(await btcTxHelper.getAddressBalance(federationAddress));
-    expect(Number(federationAddressBalanceAfterFirstPegout)).to.be.equal(Number(federationAddressBalanceInitial + 2 * MINIMUM_PEGIN_VALUE_IN_BTC));
+    expect(Number(federationAddressBalanceAfterFirstPegout)).to.be.equal(Number(federationAddressBalanceInitial + PEGIN_VALUE_IN_BTC - PEGOUT_VALUE_IN_RBTC));
 
     const senderAddressBalanceAfterFirstPegout = Number(await btcTxHelper.getAddressBalance(btcAddressInfo.address));
-    expect(Number(senderAddressBalanceAfterFirstPegout)).to.be.above(MINIMUM_PEGIN_VALUE_IN_BTC - btcTxHelper.getFee()).and.below(MINIMUM_PEGIN_VALUE_IN_BTC);
+    expect(Number(senderAddressBalanceAfterFirstPegout)).to.be.above(PEGOUT_VALUE_IN_RBTC - btcTxHelper.getFee()).and.below(PEGOUT_VALUE_IN_RBTC);
 
-    const recipientRskAddressAfterFirstPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address));
-    expect(Number(recipientRskAddressAfterFirstPegout)).to.be.above(PEGOUT_VALUE_IN_WEIS - RSK_TX_FEE_IN_RBTC).and.below(2 * PEGOUT_VALUE_IN_WEIS);
+    const recipientRskAddressBalanceAfterFirstPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address));
+    expect(Number(recipientRskAddressBalanceAfterFirstPegout)).to.be.above(btcEthUnitConverter.btcToWeis(PEGIN_VALUE_IN_BTC - PEGOUT_VALUE_IN_RBTC - RSK_TX_FEE_IN_RBTC)).and.below(btcToWeis(PEGIN_VALUE_IN_BTC - PEGOUT_VALUE_IN_RBTC));
     
     await sendTxToBridge(rskTxHelper, PEGOUT_VALUE_IN_RBTC, recipientRskAddressInfo.address);
     await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
 
     const senderAddressBalanceAfterSecondPegout = Number(await btcTxHelper.getAddressBalance(btcAddressInfo.address));
-    expect(Number(senderAddressBalanceAfterSecondPegout)).to.be.above(FINAL_SENDER_BALANCE_APPROX).and.below(2 * PEGOUT_VALUE_IN_RBTC);
+    expect(Number(senderAddressBalanceAfterSecondPegout)).to.be.above(senderAddressBalanceAfterFirstPegout + PEGOUT_VALUE_IN_RBTC - btcTxHelper.getFee()).and.below(senderAddressBalanceAfterFirstPegout + PEGOUT_VALUE_IN_RBTC);
 
-    const recipientRskAddressAfterSecondPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address));
-    expect(Number(recipientRskAddressAfterSecondPegout)).to.be.below(PEGOUT_VALUE_IN_WEIS);
+    const recipientRskAddressBalanceAfterSecondPegout = Number(await rskTxHelper.getBalance(recipientRskAddressInfo.address));
+    expect(Number(recipientRskAddressBalanceAfterSecondPegout)).to.be.above(recipientRskAddressBalanceAfterFirstPegout - btcToWeis(PEGOUT_VALUE_IN_RBTC - RSK_TX_FEE_IN_RBTC)).and.below(recipientRskAddressBalanceAfterFirstPegout - btcToWeis(PEGOUT_VALUE_IN_RBTC));
   });
 });
 
