26483e44a59b6d919d6c3c8df6c1a03c9e294815
jeremy-then <jeremy.then@iovlabs.org>
2023-12-01 12:22:22 -0400

    Renames testRunner to multipleTestExecutionsRunner and clean env before each test execution

--- multipleTestExecutionsRunner.js
+++ multipleTestExecutionsRunner.js
@@ -6,13 +6,22 @@ const timesArg = Number(process.argv[2]);
 
 const RUN_ALL_TESTS_THESE_TIMES = timesArg || Number(process.env.RUN_ALL_TESTS_THESE_TIMES) || 1;
 
-console.info(`Will attempt to run tests ${RUN_ALL_TESTS_THESE_TIMES} times.`)
+console.info(`Will attempt to run tests ${RUN_ALL_TESTS_THESE_TIMES} times.`);
+
+const cleanEnvCommand = "kill $(ps -A | grep -e java -e python -e bitcoind | awk '{print $1}')";
+
+const ensureCleanEnv = () => {
+    console.info('Cleaning environment...');
+    shell.exec(cleanEnvCommand);
+    console.info('Environment clean.');
+};
 
 let fails = 0;
 let attempts = 1;
 
 for(let i = 0; i < RUN_ALL_TESTS_THESE_TIMES; i++) {
     console.info(`Running tests ${attempts} out of ${RUN_ALL_TESTS_THESE_TIMES} times.`);
+    ensureCleanEnv();
     if (shell.exec('npm run test-fail-fast').code !== 0) {
         fails++;
     }
