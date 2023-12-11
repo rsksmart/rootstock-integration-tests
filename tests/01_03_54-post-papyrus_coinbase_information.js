1d212ec8d6acfdcc8225e630f2512d6a15c117e4
jeremy-then <jeremy.then@iovlabs.org>
2023-12-11 15:42:17 -0400

    Adds fix to wait for txs to get to the mempool before mining

--- tests/01_03_54-post-papyrus_coinbase_information.js
+++ tests/01_03_54-post-papyrus_coinbase_information.js
@@ -31,6 +31,7 @@ describe('Calling coinbase information methods after papyrus', () => {
         const blockHash = await btcClient.mine(1);
         await wait(1000);
         await rskTxHelper.updateBridge();
+        await rskUtils.waitForRskMempoolToGetTxs(rskTxHelper);
 
         const blockData = await btcClient.nodeClient.getBlock(blockHash[0], false);
         const block = bitcoinJs.Block.fromHex(blockData);
