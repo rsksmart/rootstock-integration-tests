872a107460bda4818f609d5ef63ab4760118b09e
Jan Pikora <honza@Jans-MacBook-Pro.local>
2023-12-21 17:49:14 +0100

    adding assertions

--- tests/01_03_52-disable_lock_whitelist.js
+++ tests/01_03_52-disable_lock_whitelist.js
@@ -3,7 +3,7 @@ const rskUtils = require('../lib/rsk-utils');
 const { getRskTransactionHelpers } = require('../lib/rsk-tx-helper-provider');
 const { getBtcClient } = require('../lib/btc-client-provider');
 const { getBridge, getLatestActiveForkName } = require('../lib/precompiled-abi-forks-util');
-const { satoshisToBtc } = require('@rsksmart/btc-eth-unit-converter');
+const { satoshisToBtc, btcToSatoshis } = require('@rsksmart/btc-eth-unit-converter');
 const { sendPegin, ensurePeginIsRegistered } = require('../lib/2wp-utils');
 
 const { WHITELIST_CHANGE_PK, WHITELIST_CHANGE_ADDR} = require('../lib/assertions/whitelisting')
@@ -12,8 +12,11 @@ let rskTxHelpers;
 let rskTxHelper;
 let btcTxHelper;
 let bridge;
-let MINIMUM_PEGIN_VALUE_IN_BTC;
 let federationAddress;
+let MINIMUM_PEGIN_VALUE_IN_BTC;
+let MINIMUM_PEGIN_VALUE_IN_SATS;
+let WHITELIST_DISABLE_BLOCK_DELAY;
+let FEE_IN_SATOSHI;
 
 const fulfillRequirementsToRunAsSingleTestFile = async () => {
   await rskUtils.activateFork(Runners.common.forks.papyrus200);
@@ -22,6 +25,10 @@ const fulfillRequirementsToRunAsSingleTestFile = async () => {
 describe('Disable whitelisting', function() {
 
   before(async () => {
+    if(process.env.RUNNING_SINGLE_TEST_FILE) {
+      await fulfillRequirementsToRunAsSingleTestFile();
+    }
+    
     rskTxHelpers = getRskTransactionHelpers();
     rskTxHelper = rskTxHelpers[0];
     btcTxHelper = getBtcClient();
@@ -29,42 +36,59 @@ describe('Disable whitelisting', function() {
     const latestActiveForkName = await getLatestActiveForkName();
     bridge = getBridge(rskTxHelper.getClient(), latestActiveForkName);
     federationAddress = await bridge.methods.getFederationAddress().call();
-    const minPeginValueInSatoshis = await bridge.methods.getMinimumLockTxValue().call();
-    MINIMUM_PEGIN_VALUE_IN_BTC = Number(satoshisToBtc(minPeginValueInSatoshis));
-
-    if(process.env.RUNNING_SINGLE_TEST_FILE) {
-      await fulfillRequirementsToRunAsSingleTestFile();
-    }
+    MINIMUM_PEGIN_VALUE_IN_SATS = await bridge.methods.getMinimumLockTxValue().call();
+    MINIMUM_PEGIN_VALUE_IN_BTC = Number(satoshisToBtc(MINIMUM_PEGIN_VALUE_IN_SATS));
+    WHITELIST_DISABLE_BLOCK_DELAY = 20;
+    FEE_IN_SATOSHI = btcToSatoshis(btcTxHelper.getFee())
   });
 
   it('should disable lock whitelist', async () => {
     const btcAddressInfo = await btcTxHelper.generateBtcAddress('legacy');
-    await btcTxHelper.fundAddress(btcAddressInfo.address, 2 * MINIMUM_PEGIN_VALUE_IN_BTC + btcTxHelper.getFee());
-    const btcAddressInfoBalanceInitial = Number(await btcTxHelper.getAddressBalance(btcAddressInfo.address));
-    expect(btcAddressInfoBalanceInitial).to.be.equal(2 * MINIMUM_PEGIN_VALUE_IN_BTC + btcTxHelper.getFee());
+    const btcAmountToFund = 2 * MINIMUM_PEGIN_VALUE_IN_BTC + 2 * btcTxHelper.getFee();
+    await btcTxHelper.fundAddress(btcAddressInfo.address, btcAmountToFund);
+    const btcAddressBalanceInitial = Number(btcToSatoshis(await btcTxHelper.getAddressBalance(btcAddressInfo.address)));
+    expect(btcAddressBalanceInitial).to.be.equal(Number(btcToSatoshis(btcAmountToFund)));
 
     const federationAddressBalanceInitial = Number(await btcTxHelper.getAddressBalance(federationAddress));
 
     // address is not whitelisted
     await sendPegin(rskTxHelper, btcTxHelper, btcAddressInfo, MINIMUM_PEGIN_VALUE_IN_BTC);
+    const btcAddressBalanceAfterFirstPegin = Number(btcToSatoshis(await btcTxHelper.getAddressBalance(btcAddressInfo.address)));
+    expect(btcAddressBalanceAfterFirstPegin).to.be.equal(btcAddressBalanceInitial - MINIMUM_PEGIN_VALUE_IN_SATS - FEE_IN_SATOSHI);
+    const federationAddressBalanceAfterFirstPegin = Number(await btcTxHelper.getAddressBalance(federationAddress));
+    expect(federationAddressBalanceAfterFirstPegin).to.be.equal(federationAddressBalanceInitial + MINIMUM_PEGIN_VALUE_IN_BTC)
+    
     // wait for the btc to come back so we can use the sendPegin method again
     await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
+    const btcAddressBalanceAfterFirstTriggerRelease = Number(btcToSatoshis(await btcTxHelper.getAddressBalance(btcAddressInfo.address)));
+    expect(btcAddressBalanceInitial - btcAddressBalanceAfterFirstTriggerRelease).to.be.at.most(FEE_IN_SATOSHI * 2)
+    const federationAddressBalanceAfterFirstTriggerRelease = Number(await btcTxHelper.getAddressBalance(federationAddress));
+    expect(federationAddressBalanceAfterFirstTriggerRelease).to.be.equal(federationAddressBalanceInitial)
 
     // disable whitelisting after 20 blocks
     const unlocked = await rskUtils.getUnlockedAddress(rskTxHelper, WHITELIST_CHANGE_PK, WHITELIST_CHANGE_ADDR);
     expect(unlocked).to.be.true;
-    const disableLockWhitelistMethod = bridge.methods.setLockWhitelistDisableBlockDelay(20);
+    const disableLockWhitelistMethod = bridge.methods.setLockWhitelistDisableBlockDelay(WHITELIST_DISABLE_BLOCK_DELAY);
     const disableResultCallback = (disableResult) => expect(Number(disableResult)).to.equal(1);
     await rskUtils.sendTxWithCheck(rskTxHelper, disableLockWhitelistMethod, WHITELIST_CHANGE_ADDR, disableResultCallback);
 
-    await btcTxHelper.mine(10);
+    await btcTxHelper.mine(WHITELIST_DISABLE_BLOCK_DELAY / 2);
     await rskUtils.waitAndUpdateBridge(rskTxHelper);
 
     // address is still not able to send btc to bridge after 10 blocks
     await sendPegin(rskTxHelper, btcTxHelper, btcAddressInfo, MINIMUM_PEGIN_VALUE_IN_BTC);
+    const btcAddressBalanceAfterSecondPegin = Number(btcToSatoshis(await btcTxHelper.getAddressBalance(btcAddressInfo.address)));
+    expect(btcAddressBalanceAfterFirstTriggerRelease - btcAddressBalanceAfterSecondPegin).to.be.at.most(Number(MINIMUM_PEGIN_VALUE_IN_SATS + FEE_IN_SATOSHI * 2))
+    const federationAddressBalanceAfterSecondPegin = Number(await btcTxHelper.getAddressBalance(federationAddress));
+    expect(federationAddressBalanceAfterSecondPegin).to.be.equal(federationAddressBalanceInitial + MINIMUM_PEGIN_VALUE_IN_BTC)
+    
     await rskUtils.triggerRelease(rskTxHelpers, btcTxHelper);
+    const btcAddressBalanceAfterSecondTriggerRelease = Number(btcToSatoshis(await btcTxHelper.getAddressBalance(btcAddressInfo.address)));
+    expect(btcAddressBalanceAfterFirstTriggerRelease - btcAddressBalanceAfterSecondTriggerRelease).to.be.at.most(FEE_IN_SATOSHI * 2)
+    const federationAddressBalanceAfterSecondTriggerRelease = Number(await btcTxHelper.getAddressBalance(federationAddress));
+    expect(federationAddressBalanceAfterSecondTriggerRelease).to.be.equal(federationAddressBalanceInitial)
 
-    await btcTxHelper.mine(10);
+    await btcTxHelper.mine(WHITELIST_DISABLE_BLOCK_DELAY / 2);
     await rskUtils.waitAndUpdateBridge(rskTxHelper);
 
     // after 20 blocks the whitelist period has ended and we can send money to the bridge
