c920d9581d0bbe022d968daddd0f53dfd3ae4781
jeremy-then <jeremy.then@iovlabs.org>
2024-01-03 09:36:43 -0400

    Using the last fed after the fed change.

--- tests/05_03_55-last_fork_active_call_receive_header.js
+++ tests/05_03_55-last_fork_active_call_receive_header.js
@@ -1,3 +1,3 @@
 const twoWpTests = require('../lib/tests/post_iris_call_receive_header');
 
-twoWpTests.execute('Calling receiveHeader after federation change', () => Runners.hosts.federate.host);
+twoWpTests.execute('Calling receiveHeader after federation change', () => Runners.hosts.federates[Runners.hosts.federates.length - 1].host);
