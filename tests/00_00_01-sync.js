e0155836d0abca8820620d10f1368b53abc3ee8d
jeremy-then <jeremy.then@iovlabs.org>
2024-04-24 21:53:04 -0400

    Check docker in pipeline

--- tests/00_00_01-sync.js
+++ tests/00_00_01-sync.js
@@ -4,11 +4,22 @@ const rskUtils = require('../lib/rsk-utils');
 const { expect, assert } = require('chai');
 const { getRskTransactionHelpers } = require('../lib/rsk-tx-helper-provider');
 
+const { promisify } = require('util');
+const { exec } = require('child_process');
+
+const execPromise = promisify(exec);
+
 const WAIT_IN_MILLISECONDS = 5000;
 
 describe('Federators sync', () => {
 
   it('should sync all rsk federator nodes when one of them manually mines', async () => {
+
+    const { stdout, stderr } = await execPromise('docker --version');
+
+    console.log('docker installation response: ', stdout);
+    console.log('docker installation response stderr: ', stderr);
+
     try {
       await wait(WAIT_IN_MILLISECONDS);
       expect(Runners.hosts.federates.length).to.be.greaterThan(0, 'Federates array cannot be empty');
