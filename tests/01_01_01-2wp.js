7dde46d654f919dec4dcac963f8e242c4fb7465e
jeremy-then <jeremy.then@iovlabs.org>
2024-10-10 09:14:21 -0400

    POC of test file refactor

--- tests/01_01_01-2wp.js
+++ tests/01_01_01-2wp.js
@@ -1,3 +1,2 @@
-const twoWpTests = require('../lib/tests/2wp');
+require('../lib/tests/2wp');
 
-twoWpTests.execute('BTC <=> RSK 2WP', () => Runners.hosts.federate.host);
--- tests/01_01_01-pre_orchid_2wp.js
+++ tests/01_01_01-pre_orchid_2wp.js
@@ -1,3 +1,3 @@
-const twoWpTests = require('../lib/tests/2wp-legacy');
-
-twoWpTests.execute('BTC <=> RSK 2WP (pre-orchid)', () => Runners.hosts.federate.host);
+// For example, trying to run the 2wp.js test file again would require to delete the cache of the 2wp.js file in require.
+delete require.cache[require.resolve('../lib/tests/2wp')];
+require('../lib/tests/2wp');
