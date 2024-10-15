ddf05d809b898b145caf1ac6ee8e779f74ec4e7b
Marcos Irisarri <53787863+marcos-iov@users.noreply.github.com>
2024-10-15 14:09:04 -0300

    Correct RSKIP number

--- test.js
+++ test.js
@@ -215,7 +215,7 @@ before(async () => {
       config.customConfig[`federator.amountOfHeadersToSend`] = 500;
       
       // TODO: Remove once RIT refactors have been completed
-      config.customConfig[`blockchain.config.consensusRules.rskip417`] = -1;
+      config.customConfig[`blockchain.config.consensusRules.rskip427`] = -1;
 
       // federatesToStart: [1, 2, 3]
       // additionalFederateNodes: [4, 5]
