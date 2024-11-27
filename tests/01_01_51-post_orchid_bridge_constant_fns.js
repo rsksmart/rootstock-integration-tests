6b30880b8135df1f50d1b14a53bfdb928f83e005
jeremy-then <jeremy.then@iovlabs.org>
2024-11-27 11:16:21 -0400

    Refactors bridge-calls.js to remove unnecessary test. Rebase

--- tests/01_01_51-post_orchid_bridge_constant_fns.js
+++
@@ -1,3 +0,0 @@
-const bridgeCallsTests = require('../lib/tests/bridge-calls');
-
-bridgeCallsTests.execute('Bridge calls and txs from contracts to constant fns', () => Runners.hosts.federate.host, true);
