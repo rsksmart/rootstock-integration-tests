36a78b0c66e00aa0f223b9f2ed7da352b012ac22
jeremy-then <jeremy.then@iovlabs.org>
2024-11-29 18:41:14 -0400

    Removes intermediate receive header tests since they are not needed, they are duplicate now.

--- tests/03_00_02-call_receive_headers.js
+++
@@ -1,3 +0,0 @@
-const receiveHeadersTests = require('../lib/tests/call_receive_headers');
-
-receiveHeadersTests.execute('Calling receiveHeaders last fork is active', () => Runners.hosts.federate.host);
