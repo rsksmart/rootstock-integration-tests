d24f393c79d973d26b5527217880c7bc44ade904
jeremy-then <jeremy.then@iovlabs.org>
2025-02-08 15:26:18 -0400

    Adds SVP tests and config.

--- configure.sh
+++ configure.sh
@@ -7,3 +7,7 @@ chmod 400 genesis-federation/fed3.key
 chmod 400 second-federation/fed1.key
 chmod 400 second-federation/fed2.key
 chmod 400 second-federation/fed3.key
+
+chmod 400 third-federation/fed1.key
+chmod 400 third-federation/fed2.key
+chmod 400 third-federation/fed3.key
--- tests/05_00_01-2wp-after-svp-federation-change.js
+++ tests/05_00_01-2wp-after-svp-federation-change.js
@@ -1,5 +1,5 @@
 const twoWpTests = require('../lib/tests/2wp');
 
 twoWpTests.execute(
-  'BTC <=> RSK 2WP after latest fork activates'
+  'BTC <=> RSK 2WP after svp federation change'
 );
