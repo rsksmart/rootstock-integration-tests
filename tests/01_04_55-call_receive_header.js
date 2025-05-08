d4b9299d0600286c3154ee6c35cf65adefe1c88a
jeremy-then <jeremy.then@iovlabs.org>
2025-05-08 11:00:45 -0400

    Remove unnecessary parameter

--- tests/01_04_55-call_receive_header.js
+++ tests/01_04_55-call_receive_header.js
@@ -1,3 +1,3 @@
 const receiveHeaderTests = require('../lib/tests/call_receive_header');
 
-receiveHeaderTests.execute('Calling receiveHeader', () => Runners.hosts.federate.host);
+receiveHeaderTests.execute('Calling receiveHeader');
