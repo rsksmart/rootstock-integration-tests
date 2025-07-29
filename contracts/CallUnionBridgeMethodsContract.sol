88ad73758d68dc997098c62bb439732e4d518432
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-07-29 16:53:39 -0400

    WIP

--- contracts/CallUnionBridgeMethodsContract.sol
+++ contracts/CallUnionBridgeMethodsContract.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.0;
 
 interface BridgeInterface {
-    function requestUnionRBTC(uint256 amountInWeis) external returns (int256);
-    function releaseUnionRBTC() external payable returns (int256);
+    function requestUnionBridgeRbtc(uint256 amountInWeis) external returns (int256);
+    function releaseUnionBridgeRbtc() external payable returns (int256);
 }
 
 contract CallUnionBridgeMethodsContract {
-    BridgeInterface public bridgeContract = BridgeInterface(0x0000000000000000000000000000000001000006);
+    BridgeInterface public bridge = BridgeInterface(0x0000000000000000000000000000000001000006);
 
-    function requestUnionRBTC(uint256 amountInWeis) external returns (int256) {
-        return bridgeContract.requestUnionRBTC(amountInWeis);
+    function requestUnionRBTC(uint256 _amountInWeis) external returns (int256) {
+        return bridge.requestUnionBridgeRbtc(_amountInWeis);
     }
 
     function releaseUnionRBTC() external payable returns (int256) {
-        return bridgeContract.releaseUnionRBTC{value: msg.value}();
+        return bridge.releaseUnionBridgeRbtc{value: msg.value}();
     }
+
+    receive() external payable {}
 }
