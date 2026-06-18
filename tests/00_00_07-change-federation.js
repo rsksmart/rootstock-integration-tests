8ec449fdc9081f7098f6d145b20593d52d2db1a9
Marcos <marcos@rootstocklabs.com>
2026-06-18 11:27:24 -0300

    remove first federation chage, since genesis ederation is already witness

--- tests/00_00_07-change-federation.js
+++
@@ -1,6 +0,0 @@
-const federationChangeTests = require('../lib/tests/change-federation');
-
-federationChangeTests.execute(
-    'Initial Federation change from genesis to p2wsh',
-    Runners.config.federations.secondFederation
-);
