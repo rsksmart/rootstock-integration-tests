9970a5224eba3bd9995b2bddecf46b58f45e16e9
Marcos <marcos@rootstocklabs.com>
2026-06-18 13:08:43 -0300

    Change federation at startup to include tcp signer

---
+++ tests/00_00_07-change-federation.js
@@ -0,0 +1,6 @@
+const federationChangeTests = require('../lib/tests/change-federation');
+
+federationChangeTests.execute(
+    'Initial Federation change from genesis to p2wsh',
+    Runners.config.federations.secondFederation
+);
